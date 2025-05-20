9bd5f1dfc55102b0522500a6f2ada71b269fa4a0
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2025-05-20 17:26:50 -0300

    feat: add int 0x12 to get low mem

--- hello/main.c
+++ hello/main.c
@@ -1,7 +1,7 @@
 /*
  *    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
  *    SPDX-FileCopyrightText: 2025 Gabriel Cruz <gabrieldemelocruz@gmail.com>
- *   
+ *
  *    SPDX-License-Identifier: GPL-3.0-or-later
  *
  *  This file is a derivative of SYSeg (https://gitlab.com/monaco/syseg)
@@ -11,13 +11,22 @@
 
 #include <stdio.h>
 
-int main(void)   
-{
-  printf ("Hello World");
-  return 0;
+void itoa(char *str, int value) {
+    int i = 0;
+    for (i = 0; value != 0; i++) {
+        int digit = value % 10;
+        str[i] = '0' + digit;
+        value /= 10;
+    }
+    str[i] = '\0';
 }
 
-
+int main(void) {
+    char str[32];
+    itoa(str, memsize());
+    printf(str);
+    return 0;
+}
 
 
 
